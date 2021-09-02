295c2ef2b2ad60f1b98e9484f1e51c4243f07a1a
AlwayzGunnaGame <36088865+AlwayzGunnaGame@users.noreply.github.com>
2021-09-02 11:16:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -615,7 +615,8 @@ io.on('connection', (socket) => {
   });
   
   socket.on('disconnect', () => {
-	  console.log(fields[0].queue);
+	  console.log(fields[0].queue.includes(socket.username));
+	  //console.log(fields[0].queue[socket.username]);
     if(fields[0].queue[socket.username]){
 		delete fields[0].queue[socket.username];
 	}else if(fields[1].queue[socket.username]){
